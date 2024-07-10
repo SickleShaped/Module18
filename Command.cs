0735585d6b6bd09de909c17d0ec4aae6961be6da
Sunny <sickleshaped@gmail.com>
2024-07-10 22:50:23 +0300

    Edit

--- Command.cs
+++ Command.cs
@@ -33,22 +33,6 @@ namespace Module18
             var vid = youtube.GetVideo(URL);
             Console.WriteLine(vid.Title);
 
-            /*
-            File.WriteAllBytes(source + vid.FullName, vid.GetBytes());
-
-            var inputFile = new MediaFile { Filename = source + vid.FullName };
-            var outputFile = new MediaFile { Filename = $"{source + vid.FullName}.mp3" };
-
-            using (var engine = new Engine())
-            {
-                engine.GetMetadata(inputFile);
-
-                engine.Convert(inputFile, outputFile);
-            }
-
-            */
-
-
             reciever.Operation();
         }
 
